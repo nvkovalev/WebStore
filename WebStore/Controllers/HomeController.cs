a7719a0e0983086d522cca3b53d1a5f0f808ff27
nvkovalev <44802459+nvkovalev@users.noreply.github.com>
2018-11-06 13:12:13 +0300

    Init

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly List<EmployeeView> _employees = new List<EmployeeView>
+        {
+            new EmployeeView
+            {
+                Id = 1,
+                FirstName = "Иван",
+                SurName = "Иванов",
+                Patronymic = "Иванович",
+                Age = 22
+            },
+            new EmployeeView
+            {
+                Id = 2,
+                FirstName = "Владислав",
+                SurName = "Петров",
+                Patronymic = "Иванович",
+                Age = 35
+            },
+            new EmployeeView
+            {
+                Id = 3,
+                FirstName = "Василий",
+                SurName = "Пупкин",
+                Patronymic = "",
+                Age = 40
+            }
+        };
+
+
+        public IActionResult Index()
+        {
+            return View(_employees);
+        }
+    }
+}
